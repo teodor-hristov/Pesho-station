ccac84d29b0d02fc149b7251ea87fbc978fa3f44
BorislavValov <borislav.valov1@gmail.com>
2019-03-24 20:18:03 +0200

    manage requests

--- Pesho-station/Pesho-station/frm_ManageRequests.Designer.cs
+++ Pesho-station/Pesho-station/frm_ManageRequests.Designer.cs
@@ -126,6 +126,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(871, 563);
             this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label5
             // 
--- Pesho-station/Pesho-station/frm_ManageRequests.cs
+++ Pesho-station/Pesho-station/frm_ManageRequests.cs
@@ -22,14 +22,22 @@ namespace Pesho_station
             this.pnl_main.AutoScroll = true;
         }
 
-        private int selectedId;
+        private int id;
 
-        public int SelectedId
+        public int Id
         {
-            get { return selectedId; }
-            set { selectedId = value; }
+            get { return id; }
+            set { id = value; }
         }
 
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PickupAddress { get; set; }
+        public string DropoffAddress { get; set; }
+        public string CarType { get; set; }
+        public int PickupTime { get; set; }
+        public int NumberPassengers { get; set; }
+        public string Note { get; set; }
 
         private void frm_ManageRequests_Load(object sender, EventArgs e)
         {
@@ -58,6 +66,21 @@ namespace Pesho_station
         private void btn_refresh_Click(object sender, EventArgs e)
         {
             LoadDataSet();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+            Id = int.Parse(row.Cells["id"].Value.ToString());
+            FullName = row.Cells["callerFullName"].Value.ToString();
+            PhoneNumber = row.Cells["callerPhone"].Value.ToString();
+            CarType = row.Cells["type"].Value.ToString();
+            NumberPassengers = int.Parse(row.Cells["passengers"].Value.ToString());
+            PickupTime = int.Parse(row.Cells["pickupTime"].Value.ToString());
+            PickupAddress = row.Cells["pickUpAdress"].Value.ToString();
+            DropoffAddress = row.Cells["DropUpAdress"].Value.ToString();
+            Note = row.Cells["driverNote"].Value.ToString();
+            MessageBox.Show(Id.ToString());
         }
     }
 }
