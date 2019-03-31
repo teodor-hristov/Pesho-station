409b2231b2f2921279b5c16e96e11945598db614
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2019-03-31 19:18:11 +0300

    map fix

--- Pesho-station/Pesho-station/Map.cs
+++ Pesho-station/Pesho-station/Map.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -10,12 +11,31 @@ namespace Pesho_station
 {
     public partial class Map : Form
     {
+        public string GetParameters(string docFile,string text)
+        {
+            var lines = File.ReadAllLines(docFile);
+
+            var firstFound = false;
+            for (int index = 0; index < lines.Count(); index++)
+            {
+                if (!firstFound && lines[index].Contains(text))
+                {
+                    firstFound = true;
+                }
+                if (firstFound && lines[index].Contains(text))
+                {
+                    //do, what you want, and exit the loop
+                    return lines[index];
+                }
+            }
+            return "";
+        }
+
         public Map()
         {
             InitializeComponent();
             this.TopLevel = false;
             this.AutoScroll = true;
-
             FixBrowserEmulation();
 
             bool designTime = LicenseManager.UsageMode == LicenseUsageMode.Designtime;
@@ -26,6 +46,7 @@ namespace Pesho_station
                 string docFile = Path.Combine(Application.StartupPath, "D:\\Git\\Pesho-station\\Pesho-station\\Pesho-station\\map.html");
                 string documentText = File.ReadAllText(docFile);
                 mapBrowser.DocumentText = documentText;
+                MessageBox.Show(GetParameters(docFile,"waypoint0"), GetParameters(docFile, "waypoint1"));
             }
         }
 
